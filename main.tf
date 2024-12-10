f9a622967fc3231aec9a9f376e2814d91d086669
Sardhara Nitesh <sardharaniteshj@gmail.com>
2024-12-10 22:17:04 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 resource "azurerm_resource_group" "rg-block" {
   name     = "rg-nitesh"
   location = "centralIndia"
-
+}
+resource "azurerm_resource_group" "rg-block1" {
+  name     = "rg-nitesh1"
+  location = "centralIndia"
 }
